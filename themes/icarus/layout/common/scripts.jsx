cd7a0537eaaafecb3ff3b1c861d28136c79dd8e6
鸽子Fmkli <68501612+fmkli@users.noreply.github.com>
2022-07-31 15:51:36 +0800

    Update scripts.jsx

--- themes/icarus/layout/common/scripts.jsx
+++ themes/icarus/layout/common/scripts.jsx
@@ -37,7 +37,7 @@ module.exports = class extends Component {
             <script src={url_for('/js/column.js')}></script>
             <Plugins site={site} config={config} page={page} helper={helper} head={false} />
             <script src={url_for('/js/main.js')} defer></script>
-            <script>!function(p){"use strict";!function(t){var s=window,e=document,i=p,c="".concat("https:"===e.location.protocol?"https://":"http://","sdk.51.la/js-sdk-pro.min.js"),n=e.createElement("script"),r=e.getElementsByTagName("script")[0];n.type="text/javascript",n.setAttribute("charset","UTF-8"),n.async=!0,n.src=c,n.id="LA_COLLECT",i.d=n;var o=function(){s.LA.ids.push(i)};s.LA?s.LA.ids&&o():(s.LA=p,s.LA.ids=[],o()),r.parentNode.insertBefore(n,r)}()}({id:"JgapU3Sla12DMquS",ck:"JgapU3Sla12DMquS"});</script>
+            <script src={url_for('/js/tongji.js')} defer></script>
         </Fragment>;
     }
 };
